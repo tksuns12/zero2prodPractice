5d5485e75c54fbd7cc0fbdcd61a7076733be407c
tksuns12 <tksuns@gmail.com>
2022-01-24 14:59:17 +0900

    Implement Simple Health Check

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[tokio::main]
@@ -11,6 +15,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .route("/", web::get().to(greet))
             .route("/{name}",web::get().to(greet))
+            .route("/health_check",web::get().to(health_check))
     })
         .bind("127.0.0.1:8000")?
         .run()

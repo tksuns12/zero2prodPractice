0e55d8a4c278f6202d52318aefa79d014a083807
tksuns12 <tksuns@gmail.com>
2022-01-24 15:43:42 +0900

    Implement First Integration Test - Modify to use random port when testing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
+
 
 }

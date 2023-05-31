6daf0b014e0fe531a63e319b4b8618217235edb2
tksuns12 <tksuns@gmail.com>
2023-05-31 20:27:03 +0900

    run() 메서드가 TcpListener를 받도록 수정

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run("127.0.0.1:0")?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,14 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
-
-    // Act
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
+    // Act
     let res = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -17,7 +18,12 @@ async fn health_check_works() {
     assert_eq!(Some(0), res.content_length());
 }
 
-async fn spawn_app() {
-    let server = zero2prod::run("127.0.0.1:0").expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server =
+        zero2prod::run(listener)
+            .expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}",port)
 }

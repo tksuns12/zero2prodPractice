dbd3e58a24009b64a604286e6e0c7b170ecf457b
tksuns12 <tksuns@gmail.com>
2023-05-31 20:16:50 +0900

    writes health_check test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,21 +1,23 @@
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app().await.expect("Failed to spawn our app");
-    
+    spawn_app();
+
     // Act
     let client = reqwest::Client::new();
 
-    let res = client.get("http://127.0.0.1:8000/health_check")
-    .send()
-    .await.expect("Failed to execute request.");
+    let res = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
 
     // Assert
     assert!(res.status().is_success());
     assert_eq!(Some(0), res.content_length());
-
 }
 
-async fn spawn_app() -> Result<(), std::io::Error> {
-    todo!()
+async fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
 }

0ce0f2d63ef57d328599892d6beeb4f8721620f8
tksuns12 <tksuns@gmail.com>
2023-06-13 08:18:33 +0900

    replace log with tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,13 +10,13 @@ use crate::FormData;
 
 pub async fn subscribe(form: Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
+    tracing::info!(
         "request_id {} - Adding '{}' '{}' as a new subscriber",
         request_id,
         form.email,
         form.name
     );
-    log::info!(
+    tracing::info!(
         "request_id {} - Saving new subscriber details in the database",
         request_id
     );
@@ -34,14 +34,14 @@ pub async fn subscribe(form: Form<FormData>, pool: web::Data<PgPool>) -> HttpRes
     .await
     {
         Ok(_) => {
-            log::info!(
+            tracing::info!(
                 "request_id {} - New subscriber details have been saved",
                 request_id
             );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!(
+            tracing::error!(
                 "request_id {} - Failed to execute query: {:?}",
                 request_id,
                 e

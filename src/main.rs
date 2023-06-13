42015b443e88f3be333797f92f8f35713150e552
tksuns12 <tksuns@gmail.com>
2023-06-13 21:15:21 +0900

    make the db pool connection lazy

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,8 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres");
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+            .expect("Failed to crate Postgres connection pool");
     let address = format!("127.0.0.1:{}", configuration.application.port);
     let listener = TcpListener::bind(address).expect("Failed to bind");
     run(listener, connection_pool)?.await?;
